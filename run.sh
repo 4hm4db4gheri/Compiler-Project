4e020776b1c4cf7b1fd8bc1cf1cb8eae4b034b6b
Ahmad Bagheri <u.ahmadbagheri@gmail.com>
2025-01-04 13:07:10 +0330

    [remove] phase-1
    removed old version to add the new version of repo

--- run.sh
+++
@@ -1,7 +0,0 @@
-cd build
-cd src
-./compiler "$(cat ../../input.txt)" 
-#> compiler.ll
-#llc --filetype=obj -o=compiler.o compiler.ll
-#clang -o compilerbin compiler.o ../../rtCompiler.c
-#./compilerbin
